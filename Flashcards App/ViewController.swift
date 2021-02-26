0077020b8cc3a4b5151cd2cf40447f60040f5e42
jojococo1217 <76746953+jojococo1217@users.noreply.github.com>
2021-02-26 15:16:07 -0800

    Initial Commit

---
+++ Flashcards App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flashcards App
+//
+//  Created by Joanne Oh on 2/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
